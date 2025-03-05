982f4a5bffa82c61a0f93c2775fe8309544832c0
Raphaël Pierre <pierre.raphael@gmail.com>
2025-03-05 15:59:53 +0100

    Initial Commit

---
+++ CryptoMap/CryptoMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMapApp.swift
+//  CryptoMap
+//
+//  Created by Raphael PIERRE on 05.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
